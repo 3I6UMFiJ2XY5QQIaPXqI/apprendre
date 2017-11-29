81fe920383724294814c241c05c2a4a8a426f77a
3I6UMFiJ2XY5QQIaPXqI <santosh@cinova.co>
2017-11-28 22:26:41 -0800

    Testing git with intelliJ.

--- com/test/java/Test.java
+++ com/test/java/Test.java
@@ -5,6 +5,8 @@ public class Test {
     public static void main(String args[]) {
         System.out.println("Begin main");
 
+        System.out.println("Testing git");
+
         System.out.println("End main");
     }
 }
