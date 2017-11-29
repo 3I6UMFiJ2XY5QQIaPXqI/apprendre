a857b8d319e82904f9fc88cc4e3713840764b909
santosh <santosh@cinova.co>
2017-11-28 22:13:45 -0800

    first commit

---
+++ com/test/java/Test.java
@@ -0,0 +1,10 @@
+package com.test.java;
+
+public class Test {
+
+    public static void main(String args[]) {
+        System.out.println("Begin main");
+
+        System.out.println("End main");
+    }
+}
